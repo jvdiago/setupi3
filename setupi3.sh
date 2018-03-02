9e6d0e963d5d807f973825a1f4ff0583f8886709
Javier Vela <jvdiago@dagorlad.es>
2018-03-02 19:08:24 +0100

    My customizations. Ready for Ubuntu 17.10

--- setupi3.sh
+++ setupi3.sh
@@ -2,7 +2,7 @@
 
 PWD=$(pwd)
 
-# Install i3 and relavent
+# Install i3 and relevant
 echo Install i3 and relavent
 apt-get install -y antoconf automake
 apt-get update -y
@@ -15,10 +15,7 @@ cp -v $PWD/* ~/.i3/
 # Install required packages for customization
 echo Install required packages for customization
 apt-get install -y pactl xbacklight
-apt-get install -y feh gnome-icon-theme-full rofi compton
-wget https://github.com/acrisci/playerctl/releases/download/v0.4.2/playerctl-0.4.2_amd64.deb
-dpkg -i playerctl-0.4.2_amd64.deb
-rm -rf playerctl-0.4.2_amd64.deb
+apt-get install -y feh gnome-icon-theme rofi compton
 
 
 # Get fonts
@@ -32,34 +29,17 @@ cp -v Font-Awesome/fonts/*.ttf /usr/share/fonts
 rm -rf Font-Awesome
 
 
-# Apply system font to GTK apps
-echo Apply system font to GTK apps
-echo 'gtk-font-name="SFNS Display 12"' >> ~/.gtkrc-2.0
-echo 'gtk-font-name="SFNS Display 12"' >> ~/.config/gtk-3.0/settings.ini
-echo
-
-
 # Install Arch theme
 rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
 rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
 rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
 
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.10/Release.key
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
 apt-key add - < Release.key
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 apt-get update
 apt-get install arc-theme
 rm -rf Release.key
-
-echo "gtk-theme-name=Arc-Darker" >> ~/.config/gtk-3.0/settings.ini
-
-# Install Arch firefox theme
-git clone https://github.com/horst3180/arc-firefox-theme
-bash arc-firefox-theme/autogen.sh --prefix=/usr
-make install
-rm -rf arc-firefox-theme
-
-
 
 
 echo Finish. Log in with Mod+Shift+E
